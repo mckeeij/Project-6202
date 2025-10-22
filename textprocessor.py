03c4102a39842a23cc51c82fc6f3589c39516388
mckeeij <mckeeij.im@gmail.com>
2025-10-21 21:17:59 -0400

    Update textprocessor.py

--- textprocessor.py
+++ textprocessor.py
@@ -1,1 +1,24 @@
+'''
+This is the manually coded version of the Natural Language Processor.
+Write dialogue in notes using triple apostrophe, hash out code that's yet to be implemented.
+RECORD ALL CHANGES
+'''
 
+## EXAMPLE ##
+#import os
+#import numpy
+#import matplotlib
+#import nltk
+
+#def lexical_diversity(text)
+#  return len(text)/len(set(text)
+
+import os
+import numpy
+import matpolotlib
+import nltk
+
+nltk.download()
+
+class TextProcessor:
+  def __init__(self, filepath)
