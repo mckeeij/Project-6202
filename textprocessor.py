040d4231647c6f9dc6c79fdea237db4298839e80
mckeeij <mckeeij.im@gmail.com>
2025-11-27 19:22:13 -0500

    Update textprocessor.py
    WordNet Citation

--- textprocessor.py
+++ textprocessor.py
@@ -2,6 +2,10 @@
 This is the manually coded version of the Natural Language Processor.
 Write dialogue in notes using triple apostrophe, hash out code that's yet to be implemented.
 RECORD ALL CHANGES
+
+WordNet License (Citation)
+George A. Miller (1995). WordNet: A Lexical Database for English.
+Communications of the ACM Vol. 38, No. 11: 39-41.
 '''
 
 ## EXAMPLE ##
