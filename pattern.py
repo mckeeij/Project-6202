2d210d44841b5962525052099d4327793a3af58d
JoeyEatonCoding <jeaton10408@gmail.com>
2025-12-23 14:20:36 -0500

    Add unigram and bigram stemming functionality
    Implement unigram and bigram stemming for a list of words.

--- pattern.py
+++ pattern.py
@@ -1,1 +1,43 @@
+import numpy
+import pandas
 
+##words = pandas.read_sql("data.csv", header=None) Imports Data
+words = ['Hello','Joey','how','are','you']
+
+##Unigram Stemming
+
+unigram_list = []
+
+for word in words:
+  for letter in word:
+    unigram_list.append(letter)
+
+'''Check dictionary
+if unigram in dictionary:
+  return "already in dictionary"
+  break
+else:
+  add to dictionary
+'''
+
+print(unigram_list)
+
+##Bigram Stemming
+
+bigram_list = []
+
+for word in words:
+  modified_word = word[:-1]  # Exclude the last character
+  for i in range(len(modified_word) - 1):
+    bigram = modified_word[i:i+2]
+    bigram_list.append(bigram)
+
+'''Check dictionary
+if bigram in dictionary:
+  return "already in dictionary"
+  break
+else:
+  add to dictionary
+'''
+
+print(bigram_list)
